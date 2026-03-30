9074723321bddedbfe6beb492013ce82d503d18f
gzhcay99 <63838592+gzhcay99@users.noreply.github.com>
2026-03-30 23:05:42 +0800

    final update

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
-const CACHE_NAME = 'asset-hq-v1';
-const assetsToCache = ['./index.html', './detail.html', './style.css', './script.js', './manifest.json'];
+const CACHE_NAME = 'assethq-v2.4.1';
+const ASSETS = [
+  '/my_new_assets/',
+  '/my_new_assets/index.html',
+  '/my_new_assets/style.css',
+  '/my_new_assets/script.js',
+  '/my_new_assets/detail.html'
+];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assetsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
